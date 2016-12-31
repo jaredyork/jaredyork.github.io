4e00883d3651cdf4a6d54e397801e3b262fd4eb4
jaredyork <yorkwebdesign@gmail.com>
2016-12-31 16:18:04 -0500

    v1.0.5.4 - Updated parallax image filename for custom computer page.

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,3 @@
 $('.header-main').parallax({imageSrc: 'images/background2.jpg'});
 $('#home-services').parallax({imageSrc: 'images/background1.jpg'});
-$("#customcomputers-plans").parallax({imageSrc: 'images/background3.jpg'});
+$("#customcomputers-plans").parallax({imageSrc: 'images/background3.JPG'});
