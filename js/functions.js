34ba1f4a2a1ddd49651580bb7259285c6cfc8abc
jaredyork <yorkwebdesign@gmail.com>
2017-02-19 17:53:28 -0500

    Re-designed headers, re-worked navigation to use hamburger menu.

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,8 @@
-$('.header-main').parallax({imageSrc: 'images/background2.jpg'});
+//$('.header-main').parallax({imageSrc: 'images/header.jpg'});
+//$('.container-header-content').parallax({imageSrc: 'images/headerglass.jpg'});
 $('#home-services').parallax({imageSrc: 'images/background1.jpg'});
 $("#customcomputers-plans").parallax({imageSrc: 'images/background3.jpg'});
+
+document.querySelector(".btn-navburger").addEventListener("click", function() {
+  this.classList.toggle("active");
+});
