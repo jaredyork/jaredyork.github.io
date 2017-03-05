db3d0bdaa0d4cd25be899d6d225eb1a6eae39583
jaredyork <yorkwebdesign@gmail.com>
2017-03-05 13:57:29 -0500

    v1.0.6.11 - Made various visual tweaks, added announcement.

--- js/functions.js
+++ js/functions.js
@@ -1,7 +1,7 @@
 //$('.header-main').parallax({imageSrc: 'images/header.jpg'});
 //$('.container-header-content').parallax({imageSrc: 'images/headerglass.jpg'});
 $('#home-services').parallax({imageSrc: 'images/background1.jpg'});
-$("#customcomputers-plans").parallax({imageSrc: 'images/background3.jpg'});
+$("#customcomputers-plans").parallax({imageSrc: 'images/background1.jpg'});
 
 document.querySelector(".btn-navburger").addEventListener("click", function() {
   this.classList.toggle("active");
